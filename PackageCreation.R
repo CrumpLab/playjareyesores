6557c149d1d6551b508e8c71782c5f9e81db7131
Matt Crump <crumpmj@gmail.com>
2019-06-19 06:21:00 -0400

    initial

---
+++ PackageCreation.R
@@ -0,0 +1,45 @@
+library(usethis)
+create_package("../pname")
+use_description_defaults()
+use_gpl3_license()
+
+use_roxygen_md()
+use_readme_md()
+
+pkgdown::build_site()
+pkgdown::build_articles()
+pkgdown::clean_site()
+pkgdown::build_news()
+
+use_news_md()
+
+#function creation
+use_r("sl_semantic_space")
+
+use_rcpp(name = "rowSumsSq")
+
+use_package("lsa", "Suggests")
+usethis::use_package("tidyr")
+
+usethis::use_pipe()
+
+use_package_doc()
+use_namespace()
+
+use_vignette("Semantic_Librarian_Tutorial")
+
+
+
+### create sample data
+
+article_df <- article_df[1:100,]
+article_vectors <- article_vectors[1:100,]
+WordVectors <- WordVectors[1:100,]
+author_list <- author_list[1:100]
+dictionary_words <- dictionary_words[1:100]
+AuthorVectors <- AuthorVectors[1:100,]
+
+use_data(article_df,article_vectors,WordVectors,author_list,dictionary_words)
+use_data(AuthorVectors)
+
+
